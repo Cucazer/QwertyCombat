eb88753e3c795502b567aa8114a3e6f1c3cda5ba
Konstantins Franckevics <cucazik@inbox.lv>
2019-01-07 18:28:55 +0100

    Drafted new ship look

--- QwertyCombat/Objects/ShipScout.cs
+++ QwertyCombat/Objects/ShipScout.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Eto.Drawing;
 using QwertyCombat.Objects.Weapons;
 
@@ -25,9 +26,38 @@ namespace QwertyCombat.Objects
                 new PointF(17, 0)
             };
 
-            this.WeaponPoint = polygonPoints[2];
-            
-            this.ObjectAppearance = new List<DrawableShape> { new Polygon(new Point(0,0), new Color() , polygonPoints, true)};
+            var windowPoints = new List<PointF>
+            {
+                new PointF(-7, 0),
+                new PointF(-7, 6),
+                new PointF(9, 0)
+            };
+
+            var weaponPoints = new List<PointF>
+            {
+                new PointF(13, 1),
+                new PointF(23, 1),
+                new PointF(23, -1),
+                new PointF(13, -1)
+            };
+
+            var nozzlePoints = new List<PointF>
+            {
+                new PointF(-20, 6),
+                new PointF(-12, 6),
+                new PointF(-7, 11),
+                new PointF(-7, 18),
+                new PointF(-12, 22),
+                new PointF(-20, 22)
+            };
+
+            this.WeaponPoint = new PointF(23, 0);
+
+            this.ObjectAppearance = new List<DrawableShape> { new Polygon(new Point(0,0), new Color() , polygonPoints, true),
+                                                                new Polygon(new Point(0,0), Colors.Aqua, windowPoints),
+                                                                new Polygon(new Point(0,0), Colors.LightGrey, weaponPoints),
+                                                                new Polygon(new Point(0,0), Colors.DarkSeaGreen, nozzlePoints),
+                                                                new Polygon(new Point(0,0), Colors.DarkSeaGreen, nozzlePoints.Select(p => new PointF(p.X, -p.Y)).ToList()) };
 
             if (this.Owner == Player.SecondPlayer)
             {
